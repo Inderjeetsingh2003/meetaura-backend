f2ba8965537c8b2f59b325180f131771c5ea0932
inderjeetsingh2003 <phenomenalinderjeet2003@gmail.com>
2024-06-12 17:49:02 +0530

    backend

--- controller/roomcontroller.js
+++ controller/roomcontroller.js
@@ -3,20 +3,21 @@ const Room=require('../schema/Roomschema')
 const createroom=(async(req,res)=>
 {
     const{title,description,accesstype,roomtype}=req.body
+    console.log(req.body)
 
     try{
 
         let room=await Room.findOne({title})
             if(room)
                 {
-                    return res.status(400).json({success:0,messgage:"room with this title already exists"})
+                    return res.status(400).json({success:0,message:"room with this title already exists"})
                 }
                 room=new Room({
                     title,description,accesstype,roomtype,admin:req.user.id
                 })
         
                 await room.save()
-                return res.status(200).json({success:1,messgage:"room created successfully"})
+                return res.status(200).json({success:1,message:"room created successfully"})
     }
     catch(error){
         console.log("error is ",error.messgage)
