c7ab8627d7756b37f1a795450c7a28c274d753af
inderjeetsingh2003 <phenomenalinderjeet2003@gmail.com>
2024-06-14 02:13:29 +0530

    chat public

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 const express=require('express');
 const path = require('path');
+const {createServer} =require('node:http')
+const{Server}=require('socket.io')
 const app=express()
-const cors=require('cors')
+const cors=require('cors');
+const { METHODS } = require('http');
+
 app.use(cors())
 app.use(express.json())
 
 require("dotenv").config()
+const server=createServer(app)
+const io= new Server(server,{
+    cors:{
+        origin:'*',
+        METHODS:['POST','GET'],
+        credentials:true
+    }
+})
 require('./dbcon/dbconnect')
 const PORT=4000;
 
+io.on('connection',(socket)=>
+{
+    console.log(`${socket.id} is connected`)
+    socket.on('setusername',(data)=>
+    {
+        const{roomid,username}=data
+        socket.join(roomid)
+        socket.broadcast.to(roomid).emit('userconnected',`${username} has joined the ${roomid}`)
+    })
+    
+    socket.on('sendmessage',(data)=>
+    {
+        const{message,roomid}=data;
+        io.to(roomid).emit('message',message)
+
+    })
+
+  
+})
+
+
+
 app.use('/user',require(path.join(__dirname,'./routes/user.js')))
 app.use('/room',require(path.join(__dirname,'./routes/roomroutes.js')))
-app.listen(PORT,()=>
+
+
+
+server.listen(PORT,()=>
 {
-    console.log(`app is listening at port:${PORT}`)
+    console.log(`server is listening at port:${PORT}`)
 })
